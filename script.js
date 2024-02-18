9cfeaf48a3a3fd1ecdd606b9289b83340fc27e93
orkhan-m <orkhanmirzayev96@gmail.com>
2024-02-19 00:18:32 +0100

    create playground dimensions

--- script.js
+++ script.js
@@ -2,30 +2,67 @@
 
 const newGame = document.querySelector(".new-game-btn");
 const playground = document.querySelector(".playground");
-const circle = document.querySelectorAll(".circle");
+let circle = document.querySelectorAll(".circle");
 
 let difficulty = 1;
 const selectDifficulty = function () {
-  let difficulty = prompt(
-    "Select Difficulty: 1 - easy, 2 - medium, 3 - difficult"
+  difficulty = Number(
+    prompt("Select Difficulty: 1 - easy, 2 - medium, 3 - difficult")
   );
   return difficulty;
 };
 
 newGame.addEventListener("click", function () {
   // selectDifficulty();
-  console.log(selectDifficulty());
-  console.log(difficulty);
+  selectDifficulty();
 
   if (difficulty === 1) {
     circle.forEach((el) => el.remove());
+
+    playground.style.gridTemplateColumns = "10rem 10rem";
+    playground.style.gridTemplateRows = "10rem 10rem";
+
+    for (let i = 0; i < 4; i++) {
+      playground.insertAdjacentHTML(
+        "beforeend",
+        `<div class=circle circle-${i + 1}></div>`
+      );
+    }
+
+    circle = document.querySelectorAll(".circle");
   }
 
   if (difficulty === 2) {
     circle.forEach((el) => el.remove());
+
+    playground.style.gridTemplateColumns = "10rem 10rem 10rem 10rem";
+    playground.style.gridTemplateRows = "10rem 10rem 10rem 10rem";
+
+    for (let i = 0; i < 16; i++) {
+      playground.insertAdjacentHTML(
+        "beforeend",
+        `<div class=circle circle-${i + 1}></div>`
+      );
+    }
+
+    circle = document.querySelectorAll(".circle");
   }
 
   if (difficulty === 3) {
+    console.log(difficulty);
     circle.forEach((el) => el.remove());
+
+    playground.style.gridTemplateColumns =
+      "10rem 10rem 10rem 10rem 10rem 10rem";
+    playground.style.gridTemplateRows = "10rem 10rem 10rem 10rem 10rem 10rem";
+
+    for (let i = 0; i < 36; i++) {
+      playground.insertAdjacentHTML(
+        "beforeend",
+        `<div class="circle circle-${i + 1}"></div>`
+      );
+    }
+
+    circle = document.querySelectorAll(".circle");
   }
 });
